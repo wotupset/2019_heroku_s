ada1c2acf043912fc48281bf8e31c33ef97dbeac
alice <wotupset@gmail.com>
2019-02-28 07:17:55 +0800

    Update 180313-login_info.php

--- 180313-login_info.php
+++ 180313-login_info.php
@@ -2,9 +2,8 @@
 header("content-Type: application/json; charset=utf-8"); //強制
 
 $tmp=parse_url(getenv("DATABASE_URL"));
-print_r($tmp);
-
-print_r($_ENV);
+//print_r($tmp);//含有密碼資訊
+//print_r($_ENV);//含有密碼資訊
 
 $env=(object)[];
 $env->HTTP_HOST = ''.$_ENV['HTTP_HOST'];
@@ -12,6 +11,9 @@ $env->HTTP_USER_AGENT = ''.$_ENV['HTTP_USER_AGENT'];
 $env->SCRIPT_NAME = ''.$_ENV['SCRIPT_NAME'];
 print_r($env);
 print_r($env->HTTP_USER_AGENT);
+echo "\n";
+echo "\n";
+
 
 $arr=[
   'HTTP_HOST'=>''.$_ENV['HTTP_HOST'],
@@ -20,9 +22,7 @@ $arr=[
 ];
 print_r($arr);
 print_r($arr['SCRIPT_NAME']);
-
-
-
+echo "\n";
 echo "\n";
 
 
