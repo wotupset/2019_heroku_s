5304ba5b89198f9df125c965af408d5a0e2175fc
alice <wotupset@gmail.com>
2019-02-28 07:14:21 +0800

    Update 180313-login_info.php

--- 180313-login_info.php
+++ 180313-login_info.php
@@ -1,17 +1,28 @@
 <?php
-//header("content-Type: application/json; charset=utf-8"); //強制
-echo $tmp=parse_url(getenv("DATABASE_URL"));
-echo "\n";
+header("content-Type: application/json; charset=utf-8"); //強制
 
-$db=(object)[];
-$db->host = ''.$_ENV['MYSQL_HOST'];//'localhost'
-$db->user = ''.$_ENV['MYSQL_USERNAME'];
-$db->pass = ''.$_ENV['MYSQL_PASSWORD'];
-$db->name = ''.$_ENV['MYSQL_DBNAME'];
-$db->port = ''.$_ENV['MYSQL_PORT'];
+$tmp=parse_url(getenv("DATABASE_URL"));
+print_r($tmp);
 
 print_r($_ENV);
-echo $db->host;
+
+$env=(object)[];
+$env->HTTP_HOST = ''.$_ENV['HTTP_HOST'];
+$env->HTTP_USER_AGENT = ''.$_ENV['HTTP_USER_AGENT'];
+$env->SCRIPT_NAME = ''.$_ENV['SCRIPT_NAME'];
+print_r($env);
+print_r($env->HTTP_USER_AGENT);
+
+$arr=[
+  'HTTP_HOST'=>''.$_ENV['HTTP_HOST'],
+  'HTTP_USER_AGENT'=>''.$_ENV['HTTP_USER_AGENT'],
+  'SCRIPT_NAME'=>''.$_ENV['SCRIPT_NAME'],
+];
+print_r($arr);
+print_r($arr['SCRIPT_NAME']);
+
+
+
 echo "\n";
 
 
